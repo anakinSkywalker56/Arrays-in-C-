fc9a0ce15780e6dbeab5075fab7d7d1ee6597e8f
Anakin <johninjug56@gmail.com>
2025-03-24 11:03:07 +0800

    Initial commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Arrays
+{
+    public class TestArray
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            int[] b = new int[5];
+            int[,] c = new int[2, 3];
+
+            Console.WriteLine("""
+            Select Option:
+            1. Declared array contents
+            2. Single-dimensional array
+            3. Multi-dimensional or 2D array
+            4. Jagged array 
+            """);
+
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+
+            switch (num)
+            {
+                case 1:
+                    foreach (int numbers in a)
+                    {
+                        Console.Write(numbers + " ");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
